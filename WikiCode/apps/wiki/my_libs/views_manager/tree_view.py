35c11830bb2a54ae132a80fa4e25e66fa39d9595
LazyTroll <diahorver@gmail.com>
2016-04-09 12:36:43 +0400

    Добавил отрисовку деревьев пользователя, при загрузке страницы менеджера деревьев

--- WikiCode/apps/wiki/my_libs/views_manager/tree_view.py
+++ WikiCode/apps/wiki/my_libs/views_manager/tree_view.py
@@ -2,12 +2,27 @@
 from django.shortcuts import render
 
 from .auth import check_auth
+from WikiCode.apps.wiki.models import User
+from WikiCode.apps.wiki.my_libs.trees_management.manager import WikiTree
 
 
 def get_tree_manager(request):
 
-    context = {
-        "user_data": check_auth(request),
-    }
+    user_data = check_auth(request)
+    try:
+        user = User.objects.get(email=user_data)
+        wt = WikiTree(0)
+        wt.load_tree(user.tree)
 
-    return render(request, 'wiki/tree_manager.html', context)
+        context = {
+            "user_data": user_data,
+            "preview_tree": wt.generate_html_preview(),
+            "dynamic_tree": wt.generate_html_dynamic(),
+        }
+
+        return render(request, 'wiki/tree_manager.html', context)
+    except User.DoesNotExist:
+        context = {
+            "user_data": user_data,
+        }
+        return render(request, 'wiki/tree_manager.html', context)
