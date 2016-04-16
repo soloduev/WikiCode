436aefa4f38943049ef43e7476a0defd5204e724
LazyTroll <diahorver@gmail.com>
2016-04-17 01:19:29 +0300

    Add AGPL

--- WikiCode/apps/wiki/my_libs/trees_management/manager.py
+++ WikiCode/apps/wiki/my_libs/trees_management/manager.py
@@ -1,4 +1,23 @@
-# -*- coding: utf-8 -*-
+#   # -*- coding: utf-8 -*-
+#
+#   Copyright (C) 2016 Igor Soloduev <diahorver@gmail.com>
+#
+#   This file is part of WikiCode.
+#
+#   WikiCode is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   WikiCode is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with WikiCode.  If not, see <http://www.gnu.org/licenses/>.
+
+
 
 # Менеджер по управлению деревьями в WikiCode. version 0.42:
 
@@ -579,3 +598,7 @@ class WikiTree(object):
 
         except AttributeError:
             self.__print_error("дерево не создано")
+
+    def move_publication(self, path_publication, new_path_folder):
+        """Перемещение конспекта в дереве"""
+        pass
