1b2a9cd36848df31231750a1d919db7dfacafe4b
LazyTroll <diahorver@gmail.com>
2016-04-17 19:23:45 +0300

    Реализовал фронтенд динамических кнопок справа конспекта. Первый черновой вариант, который более менее меня устроил.

--- WikiCode/apps/wiki/static/wiki/wiki_scripts/page.js
+++ WikiCode/apps/wiki/static/wiki/wiki_scripts/page.js
@@ -1,21 +1,21 @@
 /*
-    Copyright (C) 2016 Igor Soloduev <diahorver@gmail.com>
+ Copyright (C) 2016 Igor Soloduev <diahorver@gmail.com>
 
-    This file is part of WikiCode.
+ This file is part of WikiCode.
 
-    WikiCode is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+ WikiCode is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-    WikiCode is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
+ WikiCode is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU Affero General Public License
-    along with WikiCode.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ You should have received a copy of the GNU Affero General Public License
+ along with WikiCode.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /**
  * Created by Igor on 11.04.2016.
@@ -23,14 +23,14 @@
 
 (function($) {
     $(document).ready(function() {
-      var mySlidebars = new $.slidebars();
-      $('#pollSlider-button').on('click', function() {
-        mySlidebars.slidebars.open('left');
-      });
+        var mySlidebars = new $.slidebars();
+        $('#pollSlider-button').on('click', function() {
+            mySlidebars.slidebars.open('left');
+        });
 
 
     });
-  }) (jQuery);
+}) (jQuery);
 
 
 //Добавление комментария к конспекту
@@ -40,22 +40,25 @@ $("#wiki-style-btn-add-comment").click(function () {
     {
         comment_message = $("#add-comment-wiki-page").val();
         $.ajax({
-                type: "GET",
-                url: "add_comment_in_wiki_page/",
-                data:{
-                    'comment_message':''+comment_message,
-                },
-                dataType: "text",
-                cache: false,
-                success: function(data){
-                    if (data == 'ok'){
-                        location.reload();
-                    }
-                    else
-                    {
-                        //Говорим, что комментарий добавить не удалось(
-                    }
+            type: "GET",
+            url: "add_comment_in_wiki_page/",
+            data:{
+                'comment_message':''+comment_message,
+            },
+            dataType: "text",
+            cache: false,
+            success: function(data){
+                if (data == 'ok'){
+                    location.reload();
                 }
-            });
+                else
+                {
+                    //Говорим, что комментарий добавить не удалось(
+                }
+            }
+        });
     }
 });
+
+//Динамический конспект
+
